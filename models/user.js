d6f588bf6a90c6368ed837bf80e0f7d330333bd0
gokul <gokulramdevan@gmail.com>
2023-07-13 12:27:49 +0530

    user schema added

---
+++ models/user.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    mailId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    rollNum: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+    },
+    year: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    orgSubscribed: [],
+    roles: [
+      {
+        orgID: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Organization",
+        },
+        role: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("Users", userSchema);
+export default userModel;
